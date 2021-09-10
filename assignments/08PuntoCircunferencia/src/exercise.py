ebb673018ca3fc047772ef96d7c744264d478f4d
marianadiazl <a01252731@itesm.mx>
2021-09-10 17:14:44 +0000

    Examen practica 1

--- assignments/08PuntoCircunferencia/src/exercise.py
+++ assignments/08PuntoCircunferencia/src/exercise.py
@@ -1,7 +1,19 @@
 
-
+import math
 def main():
     # Escribe tu código abajo de esta línea
+    r = float(input('Introduce el radio: '))
+    x1 = float(input('Introduce x1: '))
+    y1 = float(input('Introduce y1: '))
+    x2 = float(input('Introduce x2: '))
+    y2 = float(input('Introduce y2: '))
+    d = math.sqrt((x2-x1)**2+(y2-y1)**2)
+    if d<r:
+        print("DENTRO")
+    elif d==r:
+        print("SOBRE")
+    elif d>r:
+        print("FUERA")
    
 
 if __name__ == '__main__':
